d568f6eeb51a8d2e50465f6f7759980bea8a32a7
Zachary Long <0628469@student.osceolaschools.net>
2019-01-17 16:35:32 -0500

    Enviorment start

--- lua/Modules/Command.lua
+++ lua/Modules/Command.lua
@@ -1,1 +1,20 @@
-return {}
+Command = {}
+
+Command["Help"] = {
+    Category = "User"
+    Allias = {"?"}
+
+    Desc = "Displays a list of commands you can run",
+    LongDesc = [[
+        Displays a list of commands you can run
+        Shows a description of the command, with Allias
+    ]],
+    Usage = "[Command]"
+
+    Permission = "cmds.user.help"
+    Main = function(plr, Args)
+
+    end
+}
+
+return Command
