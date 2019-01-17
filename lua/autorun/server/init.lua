a79a65e7dc8fcc6c2ccf1dcfd3fcb7d11f2c2104
Zachary Long <0628469@student.osceolaschools.net>
2019-01-17 13:41:21 -0500

    Added Module Support

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,11 +1,13 @@
-Main = {
+Settings = {
     Prefix = "!"
 }
+Core = {}
+Core.Commands = include("Modules/Command.lua")
 
 function Chat(ply, text)
     if string.sub(text, 1, 1) == Main.Prefix then
         ply:PrintMessage(3, "Response: ".. string.sub(text, 2))
-        ply:SendLua([[gui.OpenURL("http://google.com")]])
+        ply:SendLua([[gui.OpenURL("https://google.com")]])
     end
 end
 
