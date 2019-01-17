e53952e0ac597d7633322a4966e2eae34cce438d
Zachary Long <0628469@student.osceolaschools.net>
2019-01-17 17:37:08 -0500

    Added Utils support

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -2,12 +2,12 @@ Settings = {
     Prefix = "!"
 }
 Core = {}
-Core.Commands = include("Modules/Command.lua")
+Core.Utils = include("Modules/InitUtils.lua") -- Has a bunch of-you guessed it-Utils for this 
+Core.Commands = include("Modules/Command.lua") --Contains the bulk of the built in commands
 
 function Chat(ply, text)
     if string.sub(text, 1, 1) == Main.Prefix then
-        ply:PrintMessage(3, "Response: ".. string.sub(text, 2))
-        ply:SendLua([[gui.OpenURL("https://google.com")]])
+        ply:PrintMessage(3, "Message Recived: ".. string.sub(text, 2))
     end
 end
 
