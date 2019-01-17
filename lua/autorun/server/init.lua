a2c1f2f159a351555dda860300978c6649578c55
Zachary Long <0628469@student.osceolaschools.net>
2019-01-17 13:24:05 -0500

    added a space

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -4,7 +4,7 @@ Main = {
 
 function Chat(ply, text)
     if string.sub(text, 1, 1) == Main.Prefix then
-        ply:PrintMessage(3, "Response:".. string.sub(text, 2))
+        ply:PrintMessage(3, "Response: ".. string.sub(text, 2))
         ply:SendLua([[gui.OpenURL("http://google.com")]])
     end
 end
