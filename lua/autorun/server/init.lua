be93f8dac3f1ba9abe9c75f113eb4bbd17ff7278
Zachary Long <0628469@student.osceolaschools.net>
2019-01-17 13:21:23 -0500

    Added Chat Hook

---
+++ lua/autorun/server/init.lua
@@ -0,0 +1,12 @@
+Main = {
+    Prefix = "!"
+}
+
+function Chat(ply, text)
+    if string.sub(text, 1, 1) == Main.Prefix then
+        ply:PrintMessage(3, "Response:".. string.sub(text, 2))
+        ply:SendLua([[gui.OpenURL("http://google.com")]])
+    end
+end
+
+hook.Add( "PlayerSay", "ChatHook", Chat)
