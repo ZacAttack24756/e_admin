6c82a25a66e38fc2f224c9d3aecc6ec8e58bd30a
Zachary Long <0628469@student.osceolaschools.net>
2019-01-18 08:35:18 -0500

    Added "CheckGroupPerm" helper function

--- lua/Modules/Utils.lua
+++ lua/Modules/Utils.lua
@@ -1,12 +1,36 @@
 local Utils = {}
 
-Utils.ObjInArray = function(Array, Obj)
+-- Simple, yet widely used
+function ObjInArray(Array, Obj)
     for _, v in pairs(Array) do
         if v == Obj then
             return true
         end
     end
     return false
+end
+Utils.ObjInArray = ObjInArray
+-- Very important with permissions
+Utils.CheckGroupPerm = function(Groups, GroupName, Permission)
+    local Previous = {}
+    local function Loop(GN, P)
+        local TargetGroup = Groups[GN]
+        if type(TargetGroup) =="table" then
+            local CheckThis = TargetGroup:GroupHasPerm(Permission)
+            if CheckThis == true then
+                return true
+            else
+                local Result = false
+                for _, v in pairs(rawget(TargetGroup, "Inheritance"))  do
+                    if ObjectInArray(Previous, v) == false and type(Groups[v]) == "table" then
+                        table.insert(Previous, v)
+                        Result = Result or Loop()
+                    end
+                end
+                return Result
+            end
+        end
+    end
 end
 
 return Utils
