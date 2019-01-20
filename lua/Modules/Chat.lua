1bfef85f392dcdbd848b923619a4aa54884682b4
ZacAttack24756 <30960302+ZacAttack24756@users.noreply.github.com>
2019-01-20 10:58:42 -0500

    another quick fix

--- lua/Modules/Chat.lua
+++ lua/Modules/Chat.lua
@@ -4,7 +4,7 @@ Core.Settings = include("Configuration.lua")
 
 local Prefix = string.sub(Core.Settings.Prefix, 1, 1)
 
-Chat:OnChat = function( ply, text, team )
+Chat.OnChat = function( ply, text, team )
     local Text = string.explode(" ", text)
 
     if type(Text[1]) == "string" then
