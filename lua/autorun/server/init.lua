3314dfce0490d18cc630648afc267583213c6ed9
ZacAttack24756 <30960302+ZacAttack24756@users.noreply.github.com>
2019-01-20 09:00:52 -0500

    asdasdadsas

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -5,7 +5,7 @@ Core.InitUtils = include("Modules/InitUtils.lua") -- Has a bunch of-you guessed 
 Core.Commands = include("Modules/Command.lua") --Contains the bulk of the built in commands
 
 function Chat(ply, text)
-    if string.sub(text, 1, 1) == Main.Prefix then
+    if string.sub(text, 1, 1) == Core.Settings.Prefix then
         ply:PrintMessage(3, "Message Recived: ".. string.sub(text, 2))
         return "Test1234"
     end
