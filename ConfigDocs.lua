115376dadbafea294d732f2d60c6ac4aaf0b52d1
ZacAttack24756 <30960302+ZacAttack24756@users.noreply.github.com>
2019-01-20 08:35:59 -0500

    Config Change

--- ConfigDocs.lua
+++ ConfigDocs.lua
@@ -1,13 +1,13 @@
 --[[
-    Documentation for the Main Configuration File
-
+    Documentation for the Startup Configuration File
 
     ---- Main Settings ----
+
     Prefix              [String]    :   The 1 character that will be the prefix for most chat commands
     Command             [Table]
         Categories      [Array:Tab] :   An Array of User Defined Categories
-    Groups              [Array:Tab] :   The Main Group Startup Configuration (See Below)
-
+    Groups              [Array:Tab] :   The Main Group Startup Configuration (See "Group Making")
+    Chat                [Tab]       :   A Table containing settings related to chat (See "Chat Options")
 
 
 	----    Group Making    ----
@@ -22,11 +22,26 @@
 		"Default"		[Bool]		:	Specifies if the player gets this rank for Free
         "RankLadder"	[Str]		:	The Name for the Rank Ladder for the group (None means the default). A player can only be in 1 Group per Ladder
 		"Inheritance"	[Array:Str]	:	Group names this group 'inherits' permissions from. Specify "Rank" to inherit from any lower-ranking groups
-        "Override"      [Str]       :   Define a specific Override (See Below)
+        "Override"      [Str]       :   Define a specific Permission Override (See Below)
+        "TargetNumber"  [Num]       :   A number specifing ranks this rank can target, Can be configured on command basis, but this is the default for all
+        "TargetTable"   [Tab]       :   An table that specifies
+        "Options"       [Table]     :   Options (Only applies to the "Default" rank)
+        {
+            "IconName"      [Str]   :   An Icon that will be associated with this group ( http://www.famfamfam.com/lab/icons/silk/previews/index_abc.png )
+            "RankDisplay"   [Str]   :   Text that shows up if a player has the rank
+            "ColorDisplay"  [...]   :   The Color of the RankDisplay
+        }
 	}
 
-    -- Overrides --
+    -- Permission Overrides --
     From Lowest Rights to Highest Rights
-    - No Access                     :   
+    - NoAccess                      :   No Access; Removes any privilages from this Group, and Negates any Inheritaed ranks
+    - SelfAdmin                     :   Access to all commands (except lua rcon stuff), but only allowed to run on self
     - Administrator                 :   Full Access; Automatically given every permission
+
+    ----    Chat Options    ----
+    {
+        "DisplayFormat" [Array]     :   An Array of what shows up when someone chats, See Format Below
+        FORMAT: "{PLAYERNAME}" -> Name of the player; "{RANKDISPLAY}" -> RankDisplay, shown above
+    }
 --]]
