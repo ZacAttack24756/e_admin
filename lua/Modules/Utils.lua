007c5c98f1e541093661e53d54cd20ac1cf309b7
ZacAttack24756 <30960302+ZacAttack24756@users.noreply.github.com>
2019-01-21 16:07:30 -0500

    Moved things

--- lua/Modules/Utils.lua
+++ lua/Modules/Utils.lua
@@ -11,26 +11,5 @@ function ObjInArray(Array, Obj)
 end
 Utils.ObjInArray = ObjInArray
 -- Very important with permissions
-Utils.CheckGroupPerm = function(Groups, GroupName, Permission)
-    local Previous = {}
-    local function Loop(GN, P)
-        local TargetGroup = Groups[GN]
-        if type(TargetGroup) =="table" then
-            local CheckThis = TargetGroup:GroupHasPerm(Permission)
-            if CheckThis == true then
-                return true
-            else
-                local Result = false
-                for _, v in pairs(rawget(TargetGroup, "Inheritance"))  do
-                    if ObjectInArray(Previous, v) == false and type(Groups[v]) == "table" then
-                        table.insert(Previous, v)
-                        Result = Result or Loop()
-                    end
-                end
-                return Result
-            end
-        end
-    end
-end
 
 return Utils
