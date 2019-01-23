ce705c2a105d50850fcf5a10aae962d1f552b22f
Zachary Long <0628469@student.osceolaschools.net>
2019-01-23 08:51:07 -0500

    Main File Loader function added

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,6 +1,27 @@
 -- Adopted a new structure, one that is simular in function to Ulx Admin
+
 if not e_admin then
     e_admin = {}
 
-    -- Load all the good stuff
+    -- Load all the good stuff here
+    local function MasterLoadFiles(Files)
+        local LongestLength = 0
+        for i, File in pairs(Files) do
+            if LongestLength < string.len(File) then
+                LongestLength = string.len(File)
+            end
+        end
+
+        print("__" .. string.rep("_", LongestLength) .. "__")
+        print("| Loading Files" .. string.rep(" ", LongestLength - 13) .. " |")
+        print("|_" .. string.rep("_", LongestLength) .. "_|")
+        for i, File in pairs(Files) do
+            include(File)
+            local Spaces = LongestLength - string.len(File)
+            print("| Loaded " .. File .. string.rep(" ", Spaces) .. " |" )
+        end
+        print("|_" .. string.rep("_", LongestLength) .. "_|")
+    end
+
+
 end
