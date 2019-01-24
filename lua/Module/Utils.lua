aaf3224b37385d90a12d0219c44ae6a8d816c198
Zachary Long <0628469@student.osceolaschools.net>
2019-01-24 08:24:45 -0500

    Added some basic Utilities functions

--- lua/Module/Utils.lua
+++ lua/Module/Utils.lua
@@ -1,3 +1,24 @@
 if e_admin then
-    print("aaaaaaaaaa")
+
+local Utils = {}
+
+Utils.ObjInArray = function(Array, Obj)
+    for _, v in pairs(Array) do
+        if v == Obj then
+            return true
+        end
+    end
+    return false
+end
+Utils.IndexInTable = function(Table, Index)
+    for i, _ in pairs(Table) do
+        if i == Index then
+            return true
+        end
+    end
+    return false
+end
+
+-- Export anything to the Main Runtime
+e_admin.Utils = Utils
 end
