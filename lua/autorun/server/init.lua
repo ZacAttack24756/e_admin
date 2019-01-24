ecbeb79f64ece98e1a73e8059c97ae1988643b24
Zachary Long <0628469@student.osceolaschools.net>
2019-01-24 08:26:12 -0500

    House Keeping

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,39 +1,40 @@
 -- Adopted a new structure, one that is simular in function to Ulx Admin
 
 if not e_admin then
-    e_admin = {}
 
-    -- Load all the good stuff here
-    local function MasterLoadFiles(Files)
-        local LongestLength = 0
-        for i, File in pairs(Files) do
-            if LongestLength < string.len(File) then
-                LongestLength = string.len(File)
-            end
-        end
-        LongestLength = math.max(LongestLength, 13)
+e_admin = {}
 
-        print("__" .. string.rep("_", LongestLength) .. "__")
-        print("| Loading Files" .. string.rep(" ", LongestLength - 13) .. " |")
-        print("|_" .. string.rep("_", LongestLength) .. "_|")
-        for i, File in pairs(Files) do
-            include(File)
-            local Spaces = LongestLength - string.len(File)
-            print("| Loaded " .. File .. string.rep(" ", Spaces) .. " |" )
+-- Load all the good stuff here
+local function MasterLoadFiles(Files)
+    local LongestLength = 0
+    for i, File in pairs(Files) do
+        if LongestLength < string.len(File) then
+            LongestLength = string.len(File)
         end
-        print("|_" .. string.rep("_", LongestLength) .. "_|")
     end
+    LongestLength = math.max(LongestLength, 13)
 
-    local LoadArray = {
-        "Configuration.lua",
-        "Module/Utils.lua",
-    }
-    MasterLoadFiles(LoadArray)
+    print("__" .. string.rep("_", LongestLength) .. "__")
+    print("| Loading Files" .. string.rep(" ", LongestLength - 13) .. " |")
+    print("|_" .. string.rep("_", LongestLength) .. "_|")
+    for i, File in pairs(Files) do
+        include(File)
+        local Spaces = LongestLength - string.len(File)
+        print("| Loaded " .. File .. string.rep(" ", Spaces) .. " |" )
+    end
+    print("|_" .. string.rep("_", LongestLength) .. "_|")
+end
 
-    --"______________________________"
-    --"| Loading Files              |"
-    --"|____________________________|"
-    --"| Loaded Configuration.lua   |"
-    --"| Loaded Module/test.lua     |"
-    --"|____________________________|"
+local LoadArray = {
+    "Configuration.lua",
+    "Module/Utils.lua",
+}
+MasterLoadFiles(LoadArray)
+
+--"______________________________"
+--"| Loading Files              |"
+--"|____________________________|"
+--"| Loaded Configuration.lua   |"
+--"| Loaded Module/test.lua     |"
+--"|____________________________|"
 end
