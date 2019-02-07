81259906022be516be0ff8f1bd0aa940cb391951
Zachary Long <0628469@student.osceolaschools.net>
2019-02-07 07:27:44 -0500

    Added EntityDataField; healthRegen Done

--- lua/module/Utils.lua
+++ lua/module/Utils.lua
@@ -48,6 +48,44 @@ function DeepCopy(orig, copies)
 end
 Utils.TableCopy = function(Table) return DeepCopy(Table) end
 
+-- https://forum.facepunch.com/f/gmoddev/nfom/Health-Regeneration/1/
+local DF_READ = 1
+local DF_WRITE = 2
+local DF_BOTH = 3
+Utils.EntityDataField = function( ent, key, name, force, fl )
+    fl = fl or DF_BOTH
+
+    if bit.band( fl, DF_READ ) ~= 0 then
+        ent[ "Get" .. name ] = function( e )
+            local res = e:GetSaveTable( )[ key ]
+
+            if force == FORCE_NUMBER then
+                return tonumber( res )
+            elseif force == FORCE_STRING then
+                return tostring( res )
+            elseif force == FORCE_BOOL then
+                return tobool( res )
+            else
+                return res
+            end
+        end
+    end
+
+    if bit.band( fl, DF_WRITE ) ~= 0 then
+        ent[ "Set" .. name ] = function( e, v )
+            if force == FORCE_NUMBER then
+                e:SetSaveValue( key, tonumber( v ) or 0 )
+            elseif force == FORCE_STRING then
+                e:SetSaveValue( key, tostring( v ) )
+            elseif force == FORCE_BOOL then
+                e:SetSaveValue( key, tobool( v ) )
+            else
+                e:SetSaveValue( key, v )
+            end
+        end
+    end
+end
+
 -- Export anything to the Main Runtime
 e_admin.Utils = Utils
 
--- lua/modules/heathRegen.lua
+++ lua/modules/heathRegen.lua
@@ -2,21 +2,32 @@ if e_admin then
 if e_admin.RunningConfig and e_admin.RunningConfig.Addins then
 if e_admin.RunningConfig.Modules["regenHealth"] and e_admin.RunningConfig.Modules["regenHealth"].Enabled == true then
 
-local function HPRegen()
-	local Config = e_admin.RunningConfig.Modules["regenHealth"]
+local Config = {}
 
-	for k, v in pairs(player.GetAll()) do
+local function ReloadConfig()
+	Config = e_admin.RunningConfig.Modules["regenHealth"]
+end
+
+local function Tick()
+	for k, v in ipairs(player.GetAll()) do
 		local hp = v:Health()
-		local hpDif = v:GetMaxHealth() - hp
-		if hpDif >= Config.tickHealth then
-			v:SetHealth(hp + Config.tickHealth)
-		elseif hpDif < Config.tickHealth then
-			v:SetHealth(hp + hpdif)
+		local maxhp = v:GetMaxHealth()
+
+		-- Creates the Datafield if it wasn't there before
+		if type(v:GetLastDamagedTime) ~= "function" or type(v:SetLastDamagedTime) ~= "function" then
+			e_admin.Utils.EntityDataField(v, "m_flLastDamageTime", "LastDamagedTime", FORCE_NUMBER)
+		end
+
+		if v:Alive() and hp < maxhp and v:GetLastDamagedTime() <= -(Config.tickTime) then
+			v:SetHealth(math.min(maxhp, hp + Config.tickHealth))
+			v:SetLastDamagedTime(0)
 		end
 	end
 end
 
-timer.Simple(e_admin.RunningConfig.Modules["regenHealth"].tickTime, HPRegen)
+ReloadConfig()
+Timer.Create("e_adReloadConfig", 20, 0, ReloadConfig)
+hook.Add("Tick", "Regeneration.Tick", Tick)
 
 end
 end
